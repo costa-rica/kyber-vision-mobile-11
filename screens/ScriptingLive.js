e79996966390d4cde9923f1056a7354f0ca8e393
Nick <nrodrig1@gmail.com>
2025-02-18 18:58:51 +0100

    fix: ScriptingLive missing scriptReducer functions

--- screens/ScriptingLive.js
+++ screens/ScriptingLive.js
@@ -35,6 +35,8 @@ import {
   deleteScript,
   replaceScriptActionArray,
   updateQualityPropertyInObjectOfActionsArray,
+  updateTypePropertyInObjectOfActionsArray,
+  updateSubtypePropertyInObjectOfActionsArray,
 } from "../reducers/script";
 import { useDispatch } from "react-redux";
 
